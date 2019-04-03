4f592b1c720f111eda5fbb8ab0a69a0c4c07fdff
mamunistbd <42726856+mamunistbd@users.noreply.github.com>
2019-04-03 23:24:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask("__name__")
 @app.route("/")
 def index():
-	return "hello zayan papa love u"
+	return "hello zayan papa love u a lot"
 
 @app.route("/<string:name>")
 def mm(name):
